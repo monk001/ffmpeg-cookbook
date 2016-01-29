802a0c3ecf44c425d1707131b3504c71b868c5d1
luozy3 <luozy3@ucweb.com>
2016-01-29 10:50:50 +0800

    ffmepg/build.sh optimize

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -5,12 +5,12 @@ shopt -s extglob
 # fail hard on any error
 set -e
 
-ffmpeg_archive_name=$(basename $(ls -f ffm*.tar.bz2))
-ffmpeg_archive_name="${ffmpeg_archive_name%.tar.bz2}"
-
 in_root=$PWD
 
 cd `dirname $0`
+
+ffmpeg_archive_name=$(basename $(ls -f ffm*.tar.bz2))
+ffmpeg_archive_name="${ffmpeg_archive_name%.tar.bz2}"
 
 if [[ ! -d $ffmpeg_archive_name ]]; then
   tar jxvf $ffmpeg_archive_name.tar.bz2
@@ -29,27 +29,28 @@ function die {
 # determine OS and architecture
 OS_ARCH=$(uname -sm | tr 'A-Z' 'a-z' | sed "s/\ /\-/g")
 
-# find / ask for the NDK
-echo "Looking for the android ndk root ..."
-NDK_ROOT=`which ndk-build`
-NDK_ROOT=`dirname ${NDK_ROOT}`
-#echo -n "Path to {NDK_ROOT} [${NDK_ROOT}]: "
-#read typed_ndk_root
-#test "$typed_ndk_root" && {NDK_ROOT}="$typed_ndk_root"
-
 # android level
 NDK_LEVEL=19
 
-function build_ffmpeg {
+function build_ffmpeg_android {
   echo "Building ffmpeg for android ..."
 
   cd $ffmpeg_archive_name
 
+  NDK_ROOT=`which ndk-build`
+  NDK_ROOT=`dirname ${NDK_ROOT}`
+
   #patch the configure script to use an Android-friendly versioning scheme
   #patch -u configure ${patch_root}/ffmpeg-configure.patch || die "Couldn't patch ffmpeg configure script!"
 
+  if [[ "$CPU" == "arm" ]]; then
+    TOOLCHAIN_NAME=arm-linux-androideabi
+  elif [[ "$CPU" == "x86" ]]; then
+    TOOLCHAIN_NAME=x86
+  fi
+
+  TOOLCHAIN=$(ls -d ${NDK_ROOT}/toolchains/${TOOLCHAIN_NAME}-[0-9].* | sort -r | head -1)/prebuilt/${OS_ARCH}
   SYSROOT=${NDK_ROOT}/platforms/android-${NDK_LEVEL}/arch-${CPU}
-  TOOLCHAIN=$(ls -d ${NDK_ROOT}/toolchains/${CPU}-linux-androideabi-[0-9]* | sort -r | head -1)/prebuilt/${OS_ARCH}
   PREFIX=${output_root}
 
   #**************************
@@ -71,11 +72,11 @@ function build_ffmpeg {
   #   https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
   
   if [[ "${ARCH}" = "armv5te" ]]; then
-    m_c_cxx_flags="${m_c_cxx_flags} -DHAVE_NEON=0"
+    m_c_cxx_flags="${m_c_cxx_flags}"
     m_c_cxx_flags="${m_c_cxx_flags} -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__"
     m_c_cxx_flags="${m_c_cxx_flags} -march=armv5te -mtune=xscale"
   elif [[ "${ARCH}" = "armv7a" ]]; then
-    m_c_cxx_flags="${m_c_cxx_flags} -DHAVE_NEON=1"
+    m_c_cxx_flags="${m_c_cxx_flags}"
     m_c_cxx_flags="${m_c_cxx_flags} -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
     m_ldflags="${m_ldflags} -Wl,--fix-cortex-a8"
   else
@@ -137,7 +138,7 @@ function build_ffmpeg {
 
   # build
   make clean   || die "Couldn't clean ffmpeg!"
-  make -j6     || die "Couldn't build ffmpeg!"
+  make -j$(($(nproc)-1)) || die "Couldn't build ffmpeg!"
   make install || die "Couldn't install ffmpeg!"
 
   test -d ${dist_include_root}/ffmpeg && rm -rf ${dist_include_root}/ffmpeg
@@ -158,18 +159,28 @@ function build_ffmpeg {
   cd ${top_root}
 }
 
-function build() {
-  echo "Setting up build environment for $1 ..."
+function build_ffmpeg() {
+  echo "Setting up build environment for $1 $2"
+
+  ARCH=$2
 
   # set environment variables
   top_root=$PWD
-  src_root=${top_root}/$ffmpeg_archive_name
   patch_root=${top_root}/patches
-  output_root=${top_root}/out/${ARCH}
-  dist_root=${top_root}
-  dist_bin_root=${dist_root}/bin/${ARCH}
+  src_root=${top_root}/$ffmpeg_archive_name
+  dist_root=${top_root}/$1
+  output_root=${top_root}/out/$1
+
+  dist_bin_root=${dist_root}/bin
+  dist_lib_root=${dist_root}/lib
   dist_include_root=${dist_root}/include
-  dist_lib_root=${dist_root}/lib/${ARCH}
+
+  if [[ -n "$1" ]]; then
+    output_root=${output_root}/${ARCH}
+    dist_bin_root=${dist_bin_root}/${ARCH}
+    dist_lib_root=${dist_lib_root}/${ARCH}
+    dist_include_root=${dist_include_root}/${ARCH}
+  fi
 
   # create our folder structure
   test -d ${src_root}          || mkdir -p ${src_root}
@@ -184,16 +195,22 @@ function build() {
     CPU=${ARCH}
   fi
 
-  build_$1
+  build_ffmpeg_$1
 
   echo "Build $1 done, look in ${output_root} for libraries and executables (${ARCH})."
+}
+
+function build_ffmpeg_linux() {
+  return 0
 }
 
 #ARCHES="armv5te armv7a mips x86"
 ARCHES="armv5te armv7a"
 
-for ARCH in ${ARCHES}; do
-  build ffmpeg 
-done
+#for ARCH in ${ARCHES}; do
+#  build_ffmpeg android $ARCH
+#done
+
+build_ffmpeg linux
 
 cd ${in_root}
