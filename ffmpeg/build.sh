63c008734261eb5add97dba89ce46c6ed4ca4da6
罗智勇 <lzy98794@alibaba-inc.com>
2017-12-29 20:11:19 +0800

    add old ffmpeg package

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -2,18 +2,21 @@
 
 shopt -s extglob
 
-# fail hard on any error
-set -e
-
-in_root=$PWD
-
 cd `dirname $0`
+
+# determine OS and architecture
+OS_ARCH=$(uname -sm | tr 'A-Z' 'a-z' | sed "s/\ /\-/g")
+
+# android level
+NDK_LEVEL=19
+
+COMPILER=gcc
 
 ffmpeg_archive_name=$(basename $(ls -f ffm*.tar.bz2))
 ffmpeg_archive_name="${ffmpeg_archive_name%.tar.bz2}"
 
 if [[ ! -d $ffmpeg_archive_name ]]; then
-  tar jxvf $ffmpeg_archive_name.tar.bz2
+  tar xf $ffmpeg_archive_name.tar.bz2
 fi
 
 #-- error function
@@ -26,59 +29,67 @@ function die {
   exit -1
 }
 
-# determine OS and architecture
-OS_ARCH=$(uname -sm | tr 'A-Z' 'a-z' | sed "s/\ /\-/g")
-
-# android level
-NDK_LEVEL=19
-
 function build_ffmpeg_android {
   echo "Building ffmpeg for android ..."
 
   cd $ffmpeg_archive_name
 
   NDK_ROOT=`which ndk-build`
-  NDK_ROOT=`dirname ${NDK_ROOT}`
+  NDK_ROOT=`dirname ${NDK_ROOT} 2>/dev/null`
+
+  test -d "${NDK_ROOT}" || die "ndk not found!"
+
+  #**************************
+  # apply patch
 
   #patch the configure script to use an Android-friendly versioning scheme
   #patch -u configure ${patch_root}/ffmpeg-configure.patch || die "Couldn't patch ffmpeg configure script!"
 
-  if [[ "$CPU" == "arm" ]]; then
-    TOOLCHAIN_NAME=arm-linux-androideabi
-  elif [[ "$CPU" == "x86" ]]; then
-    TOOLCHAIN_NAME=x86
+  #**************************
+  # setting
+
+  if [[ $ARCH == arm* ]]; then
+    CPU=arm
+  else
+    die "Unsupport arch ${ARCH}"
   fi
 
-  TOOLCHAIN=$(ls -d ${NDK_ROOT}/toolchains/${TOOLCHAIN_NAME}-[0-9].* | sort -r | head -1)/prebuilt/${OS_ARCH}
+  if [[ "$COMPILER" == "gcc" ]]; then
+    GCC=$(find ${NDK_ROOT}/toolchains -name "*$CPU*gcc" | sort -r | head -1)
+    test -f "${GCC}" || die "cross compiler gcc not found!"
+    CROSS_PREFIX=${GCC::-3}
+  else
+    die "Unsupport compiler $COMPILER"
+  fi
+  
   SYSROOT=${NDK_ROOT}/platforms/android-${NDK_LEVEL}/arch-${CPU}
   PREFIX=${output_root}
 
-  #**************************
-  # configure setting
+  c_cxx_flags=
+  ldflags=
+  libs=
+  disables=
+  enables=
 
-  m_c_cxx_flags=
-  m_ldflags=
-  m_libs=
-  m_disables=
-  m_enables=
-
-  m_c_cxx_flags="${m_c_cxx_flags} -marm"
-  #m_c_cxx_flags="${m_c_cxx_flags} -mthumb"
-  #m_c_cxx_flags="${m_c_cxx_flags} -fpic"
-  #m_c_cxx_flags="${m_c_cxx_flags} -ffast-math -fno-tree-vectorize"
+  if [[ "$CPU" == "arm" ]]; then
+    c_cxx_flags="${c_cxx_flags} -marm"
+  fi
+  #c_cxx_flags="${c_cxx_flags} -mthumb"
+  #c_cxx_flags="${c_cxx_flags} -fpic"
+  #c_cxx_flags="${c_cxx_flags} -ffast-math -fno-tree-vectorize"
 
   #----------------------
   # ARM Options
   #   https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
   
   if [[ "${ARCH}" = "armv5te" ]]; then
-    m_c_cxx_flags="${m_c_cxx_flags}"
-    m_c_cxx_flags="${m_c_cxx_flags} -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__"
-    m_c_cxx_flags="${m_c_cxx_flags} -march=armv5te -mtune=xscale"
+    c_cxx_flags="${c_cxx_flags}"
+    c_cxx_flags="${c_cxx_flags} -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__"
+    c_cxx_flags="${c_cxx_flags} -march=armv5te -mtune=xscale"
   elif [[ "${ARCH}" = "armv7a" ]]; then
-    m_c_cxx_flags="${m_c_cxx_flags}"
-    m_c_cxx_flags="${m_c_cxx_flags} -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
-    m_ldflags="${m_ldflags} -Wl,--fix-cortex-a8"
+    c_cxx_flags="${c_cxx_flags}"
+    c_cxx_flags="${c_cxx_flags} -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+    ldflags="${ldflags} -Wl,--fix-cortex-a8"
   else
     die "Unsupport arch ${ARCH}"
   fi
@@ -86,21 +97,21 @@ function build_ffmpeg_android {
   #------------------------------
   # configure disable and enable
 
-  #m_disables="${m_disables} --disable-muxers --disable-encoders"
-  #m_disables="${m_disables} "
+  #disables="${disables} --disable-muxers --disable-encoders"
+  #disables="${disables} "
 
-  #m_enables="${m_enables} "
+  #enables="${enables} "
 
   #------------------------------
   # others
 
-  m_cflags=${m_c_cxx_flags}
-  m_cxxflags=${m_c_cxx_flags}
+  cflags=${c_cxx_flags}
+  cxxflags=${c_cxx_flags}
 
   echo "ffmpeg configure ..."
 
   ./configure \
-    --cross-prefix=${TOOLCHAIN}/bin/${CPU}-linux-androideabi- \
+    --cross-prefix=${CROSS_PREFIX} \
     --sysroot=${SYSROOT} \
     --prefix=${PREFIX} \
     \
@@ -114,21 +125,20 @@ function build_ffmpeg_android {
     --enable-small \
     --disable-symver \
     \
-    --disable-ffmpeg \
     --disable-ffplay \
     --disable-ffprobe \
     --disable-ffserver \
     --disable-doc \
     \
-    ${m_disables} \
-    ${m_enables} \
+    ${disables} \
+    ${enables} \
     \
-    --extra-cflags="${m_cflags}" \
-    --extra-cxxflags="${m_cxxflags}" \
+    --extra-cflags="${cflags}" \
+    --extra-cxxflags="${cxxflags}" \
     \
-    --extra-ldflags="${m_ldflags}" \
+    --extra-ldflags="${ldflags}" \
     \
-    --extra-libs="${m_libs}" \
+    --extra-libs="${libs}" \
     \
     --pkg-config=$(which pkg-config) \
     || die "Couldn't configure ffmpeg!"
@@ -162,55 +172,48 @@ function build_ffmpeg_android {
 function build_ffmpeg() {
   echo "Setting up build environment for $1 $2"
 
-  ARCH=$2
-
   # set environment variables
-  top_root=$PWD
-  patch_root=${top_root}/patches
-  src_root=${top_root}/$ffmpeg_archive_name
-  dist_root=${top_root}/$1
-  output_root=${top_root}/out/$1
 
+  ARCH=$2 
+
+  top_root=$PWD
+
+  patch_root=${top_root}/patches
+
+  src_root=${top_root}/$ffmpeg_archive_name
+
+  output_root=${top_root}/out/bin/$1
+  dist_root=${top_root}/out/$1
+  if [[ -n "$ARCH" ]]; then
+    output_root=${output_root}/$ARCH
+    dist_root=${dist_root}/$ARCH
+  fi
   dist_bin_root=${dist_root}/bin
   dist_lib_root=${dist_root}/lib
   dist_include_root=${dist_root}/include
 
-  if [[ -n "$1" ]]; then
-    output_root=${output_root}/${ARCH}
-    dist_bin_root=${dist_bin_root}/${ARCH}
-    dist_lib_root=${dist_lib_root}/${ARCH}
-    dist_include_root=${dist_include_root}/${ARCH}
-  fi
-
   # create our folder structure
-  test -d ${src_root}          || mkdir -p ${src_root}
   test -d ${output_root}       || mkdir -p ${output_root}
   test -d ${dist_bin_root}     || mkdir -p ${dist_bin_root}
   test -d ${dist_include_root} || mkdir -p ${dist_include_root}
   test -d ${dist_lib_root}     || mkdir -p ${dist_lib_root}
 
-  if [[ ${ARCH} = arm* ]]; then
-    CPU=arm
-  else
-    CPU=${ARCH}
-  fi
-
   build_ffmpeg_$1
 
-  echo "Build $1 done, look in ${output_root} for libraries and executables (${ARCH})."
+  echo "Build $1 done, look in ${output_root} for libraries and executables"
 }
 
 function build_ffmpeg_linux() {
   return 0
 }
 
-#ARCHES="armv5te armv7a mips x86"
-ARCHES="armv5te armv7a"
+#ARCHES="armv5te armv7a mips x86_64"
+ARCHES="armv7a"
 
-#for ARCH in ${ARCHES}; do
-#  build_ffmpeg android $ARCH
-#done
+for ARCH in ${ARCHES}; do
+  build_ffmpeg android $ARCH
+done
 
-build_ffmpeg linux
+#build_ffmpeg linux
 
 cd ${in_root}
